fd21f4e675cdb94f2e32552d2ffc122cd7730d64
Daniel Nery <danielnso@outlook.com>
2017-04-06 17:10:39 -0300

    template tarefinha

---
+++ C/Tarefa/main.c
@@ -0,0 +1,29 @@
+/**
+ * Processo Seletivo 2017 - main.c
+ *
+ * Equipe ThundeRatz de Robótica
+ * 03/2017
+ */
+
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include "sensors.h"
+#include "motors.h"
+
+int main() {
+	sensors_init();
+	motors_init();
+	sei();
+	wdt_reset();
+	wdt_disable();
+
+	for (;;) {
+
+		_delay_ms(10);
+	}
+
+	return 0;
+}
