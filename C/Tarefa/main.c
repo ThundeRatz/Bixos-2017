4fc2609d5ba4516870cf32736d020e893196c8c6
rafgehrke <rafael.gehrke@usp.br>
2017-04-09 16:59:41 -0300

    Versão 1.0

--- C/Tarefa/main.c
+++ C/Tarefa/main.c
@@ -13,6 +13,11 @@
 #include "sensors.h"
 #include "motors.h"
 
+#define THRESHOLD_FE 50
+#define THRESHOLD_FD 50
+#define THRESHOLD_LE 400
+#define THRESHOLD_LD 400
+
 int main() {
 	sensors_init();
 	motors_init();
@@ -21,7 +26,16 @@ int main() {
 	wdt_disable();
 
 	for (;;) {
-
+		if(get_sensor(FE) < THRESHOLD_FE || get_sensor(FD) < THRESHOLD_FD){ //se tiver oponente na frente (Threshold em CM)
+			while(get_sensor(LE) > THRESHOLD_LE || get_sensor(LD) > THRESHOLD_LD){   //vai pra frente até a borda do dojo (branco é leitura pequena)
+				motors(150, 150);
+			}
+			motors(-255, -255);   //chegou até a borda do dojo. Dar um pouco de ré e repetir a rotina
+			_delay_ms(200);
+		}
+		else{                           //se não tiver oponente na frente, ficar girando
+			motors(255, -255);
+		}
 		_delay_ms(10);
 	}
 
